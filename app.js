9753a1d09d4a64a3da2b0a93f640adeb9a3f330d
Ivan <ivan.parra9@hotmail.com>
2023-11-02 14:41:37 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app  = express();
+const publicPath = path.resolve(__dirname,'./public');
+
+app.use(express.static('public'));
+app.listen(3030,() => console.log('Dale que arranca en el 3030'));
+
+//home
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve('./views/home.html'))
+});
+
